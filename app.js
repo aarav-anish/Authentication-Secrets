67127308d99196fdf7b090df1209b846e6e1dec8
Anish Kumar Barnwal <ak47barnwal@gmail.com>
2021-04-07 19:55:56 +0530

    Google OAuth 2.0 Authentication

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const mongoose = require("mongoose");
 const passport = require("passport");
 const session = require("express-session");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require('mongoose-findorcreate')
 
 const app = express();
 
@@ -34,20 +36,59 @@ db.once('open', function() {
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+  
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/", function(req, res) {
     res.render("home");
 })
+
+///////////////////// Google Authentication /////////////////////////
+
+app.get("/auth/google",
+  passport.authenticate("google", { scope: ["profile"] }
+));
+
+app.get("/auth/google/secrets", 
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/secrets");
+});
+
+//////////////////// Secrets //////////////////////
 
 app.get("/secrets", function(req, res) {
     if(req.isAuthenticated()) {
@@ -127,10 +168,10 @@ app.route("/submit")
 
 
 
-let port = process.env.PORT;
-if(port==null || port=="") {
-    port = 3000;
-}
+let port = process.env.PORT || 3000;
+
 app.listen(port, function(req, res) {
     console.log("Server started running on " + port);
+    //console.log(`Serving on port ${port}`)
+
 });
